ff1e83d815009fb927f5bc98672ac145e2e89334
Chengsi <gaochengsi@gmail.com>
2019-06-12 22:17:05 -0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import fetchItems from "./Api";
+import { fetchItems } from "./Api";
 import './App.css';
 
-function App() {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -21,24 +21,26 @@ function App() {
     this.setState({ jobs, isLoading: false });
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
