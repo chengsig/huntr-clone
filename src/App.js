404c0368b84a9ae20905724617f5d723f83b28c7
Chengsi <gaochengsi@gmail.com>
2019-06-13 16:37:40 -0700

    revamp with react hooks

--- src/App.js
+++ src/App.js
@@ -1,46 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import { fetchItems } from "./Api";
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      jobs: [],
-    };
-
-    //this.addJob = this.addJob.bind(this);
-  }
+function App() {
+  const [jobs, setJobs] = useState([]);
 
   /** Load data from backend. */
+  useEffect(() => {
+    async function fetchData() {
+      const result = await fetchItems();
+      setJobs(result.data);
+    }
+    fetchData();
+  }, []);
 
-  async componentDidMount() {
-    let jobs = await fetchItems();
-    this.setState({ jobs, isLoading: false });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    )
-  }
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
 }
 
 export default App;
